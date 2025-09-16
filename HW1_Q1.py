b6d182b818d5932d361317f73e93e30b6142b41a
ja5496 <ja5496@nyu.edu>
2025-09-16 15:37:51 -0400

    Added predicted lines

--- HW1_Q1.py
+++ HW1_Q1.py
@@ -27,7 +27,26 @@ x2 = np.float32(10.0)
 f1 = np.cos
 f2 = np.exp
 
-h = np.logspace(-7, 1, 1000).astype(np.float32) #Range of h values to test
+h = np.logspace(-7, 1, 100).astype(np.float32)
+#hsquared = np.round(h**2,7).astype(np.float32) 
+#hto4th = np.round(h**4,7).astype(np.float32) 
+
+#Define functions for predicted error:
+def predicted_err_f(h):
+    return np.abs(np.float32((10e-7)/h - h))
+
+def predicted_err_c1(h, x_i):
+    return np.abs(np.float32((10e-7)/h + np.tan(x_i)*h**2))
+
+def predicted_err_e1(h, x_i):
+    return np.abs(np.float32((10e-7)/h - np.tan(x_i)*h**4))
+
+def predicted_err_c2(h):
+    return np.abs(np.float32((10e-7)/h + h**2))
+
+def predicted_err_e2(h):
+    return np.abs(np.float32((10e-7)/h - h**4))
+
 
 print('forward diff e^x derivative of e^x at x=10', forward_diff(np.exp, x2, np.float32(1.0e-5)))
 print('actual derivative of e^x at x=10', np.exp(x2))
@@ -36,6 +55,7 @@ print('actual derivative of e^x at x=10', np.exp(x2))
 forward_err_f1_x1 = (forward_diff(f1, x1, h) + np.float32(np.sin(x1)))/np.float32(np.sin(x1))
 central_err_f1_x1 = (central_diff(f1, x1, h) + np.float32(np.sin(x1)))/np.float32(np.sin(x1))
 extrapolated_err_f1_x1 = (extrapolated_diff(f1, x1, h) + np.float32(np.sin(x1)))/np.float32(np.sin(x1))
+
 
 forward_err_f1_x2 = (forward_diff(f1, x2, h) + np.float32(np.sin(x2)))/np.float32(np.sin(x2))
 central_err_f1_x2 = (central_diff(f1, x2, h) + np.float32(np.sin(x2)))/np.float32(np.sin(x2))
@@ -50,39 +70,53 @@ forward_err_f2_x2 = (forward_diff(f2, x2, h) - np.float32(np.exp(x2)))/np.float3
 central_err_f2_x2 = (central_diff(f2, x2, h) - np.float32(np.exp(x2)))/np.float32(np.exp(x2))
 extrapolated_err_f2_x2 = (extrapolated_diff(f2, x2, h) - np.float32(np.exp(x2)))/np.float32(np.exp(x2))
 
+
 #Part B: Plot the errors for each function at each point using each method on a log-log scale:
 plt.figure(figsize=(10, 8))
 plt.subplot(2, 2, 1)
-plt.loglog(h, np.abs(forward_err_f1_x1), label='Forward Diff', linestyle='', marker='.')
-plt.loglog(h, np.abs(central_err_f1_x1), label='Central Diff', linestyle='', marker='.')
-plt.loglog(h, np.abs(extrapolated_err_f1_x1), label='Extrapolated Diff', linestyle='', marker='.')
+plt.loglog(h, np.abs(forward_err_f1_x1), label='Forward Diff')
+plt.loglog(h, np.abs(central_err_f1_x1), label='Central Diff')
+plt.loglog(h, np.abs(extrapolated_err_f1_x1), label='Extrapolated Diff')
+plt.loglog(h,predicted_err_f(h),linestyle=':', color='blue', linewidth=2)
+plt.loglog(h,predicted_err_c1(h,0.1),linestyle=':', color='orange', linewidth=2)
+plt.loglog(h,predicted_err_e1(h,0.1),linestyle=':', color='green', linewidth=2)
 plt.title('Errors for cos(x) at x=0.1',fontsize=16, fontweight='bold')
 plt.xlabel('Step size (h)',fontsize=14)
 plt.ylabel('Relative Error',fontsize=14)
 plt.legend()
 plt.grid()
 plt.subplot(2, 2, 2)
-plt.loglog(h, np.abs(forward_err_f1_x2), label='Forward Diff', linestyle='', marker='.')
-plt.loglog(h, np.abs(central_err_f1_x2), label='Central Diff', linestyle='', marker='.')
-plt.loglog(h, np.abs(extrapolated_err_f1_x2), label='Extrapolated Diff', linestyle='', marker='.')
+plt.loglog(h, np.abs(forward_err_f1_x2), label='Forward Diff')
+plt.loglog(h, np.abs(central_err_f1_x2), label='Central Diff')
+plt.loglog(h, np.abs(extrapolated_err_f1_x2), label='Extrapolated Diff')
+plt.loglog(h,predicted_err_f(h),linestyle=':', color='blue', linewidth=2)
+plt.loglog(h,predicted_err_c1(h,10),linestyle=':', color='orange', linewidth=2)
+plt.loglog(h,predicted_err_e1(h,10),linestyle=':', color='green', linewidth=2)
+plt.loglog()
 plt.title('Errors for cos(x) at x=10.0',fontsize=16, fontweight='bold')
 plt.xlabel('Step size (h)',fontsize=14)
 plt.ylabel('Relative Error',fontsize=14)
 plt.legend()
 plt.grid()
 plt.subplot(2, 2, 3)
-plt.loglog(h, np.abs(forward_err_f2_x1), label='Forward Diff', linestyle='', marker='.')
-plt.loglog(h, np.abs(central_err_f2_x1), label='Central Diff', linestyle='', marker='.')
-plt.loglog(h, np.abs(extrapolated_err_f2_x1), label='Extrapolated Diff', linestyle='', marker='.')
+plt.loglog(h, np.abs(forward_err_f2_x1), label='Forward Diff')
+plt.loglog(h, np.abs(central_err_f2_x1), label='Central Diff')
+plt.loglog(h, np.abs(extrapolated_err_f2_x1), label='Extrapolated Diff')
+plt.loglog(h,predicted_err_f(h),linestyle=':', color='blue', linewidth=2)
+plt.loglog(h,predicted_err_c2(h),linestyle=':', color='orange', linewidth=2)
+plt.loglog(h,predicted_err_e2(h),linestyle=':', color='green', linewidth=2)
 plt.title('Errors for e^x at x=0.1',fontsize=16, fontweight='bold')
 plt.xlabel('Step size (h)',fontsize=14)
 plt.ylabel('Relative Error',fontsize=14)
 plt.legend()
 plt.grid()
 plt.subplot(2, 2, 4)
-plt.loglog(h, np.abs(forward_err_f2_x2), label='Forward Diff', linestyle='', marker='.')
-plt.loglog(h, np.abs(central_err_f2_x2), label='Central Diff', linestyle='', marker='.')
-plt.loglog(h, np.abs(extrapolated_err_f2_x2), label='Extrapolated Diff', linestyle='', marker='.')
+plt.loglog(h, np.abs(forward_err_f2_x2), label='Forward Diff')
+plt.loglog(h, np.abs(central_err_f2_x2), label='Central Diff')
+plt.loglog(h, np.abs(extrapolated_err_f2_x2), label='Extrapolated Diff')
+plt.loglog(h,predicted_err_f(h),linestyle=':', color='blue', linewidth=2)
+plt.loglog(h,predicted_err_c2(h),linestyle=':', color='orange', linewidth=2)
+plt.loglog(h,predicted_err_e2(h),linestyle=':', color='green', linewidth=2)
 plt.title('Errors for e^x at x=10.0',fontsize=16, fontweight='bold')
 plt.xlabel('Step size (h)',fontsize=14)
 plt.ylabel('Relative Error',fontsize=14)
