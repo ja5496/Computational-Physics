4ab0cf4aa62f902f3c40393fed9291fbb5b1f4ce
ja5496 <ja5496@nyu.edu>
2025-10-12 17:13:54 -0400

    Q1 Plotting

--- HW3/HW3_Q1.py
+++ HW3/HW3_Q1.py
@@ -16,4 +16,21 @@ plt.xticks(fontsize = 14)
 plt.yticks(fontsize = 14)
 plt.show()
 
-#Part B
+#Part B: Calculate Fourier Transform and plot the power spectrum
+
+time = df['time'].to_numpy()
+sunspots = df['sunspots'].to_numpy()
+
+fft_vals = np.fft.rfft(sunspots)
+f = np.fft.rfftfreq(len(sunspots), d=1)       # cycles per unit time
+k = 2*np.pi*f               
+
+power = np.abs(fft_vals)**2
+
+plt.plot(f, power, linewidth=2.0)
+plt.title('Power Spectrum', fontsize=20)
+plt.xlabel('k [rad / time]', fontsize=18)
+plt.ylabel('Power |C_k|^2', fontsize=18)
+plt.ylim(-10,3e9)
+plt.xscale('log')
+plt.show()
